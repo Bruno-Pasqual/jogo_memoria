6ee20913f9cab3e41133b7c120ef7d99f32e8df0
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2022-12-20 02:56:02 -0300

    desenvolvendo a lógica do jogo

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
-/* const bolasSelecao = document.querySelectorAll('.box_bolas');
+//! Selecionando elementos para o DOM -----------------------
+
+const bolasSelecao = document.querySelectorAll('.box_bolas');
+
+//! Criando variáveis ------------------------------------
 let arr = [];
 
-bolasSelecao.forEach((value) => {
-  if (value.innerHTML === '?') {
-    let temp = Math.floor(Math.random() * 8 + 1);
-    value.innerHTML = temp;
-    arr.unshift(temp);
-  }
-});
+//! Funções -----------------------------------------------
 
-console.log(arr);
- */
-
-var arr = [2, 3, 1, 3, 4, 5, 3, 1];
-
+//FUnção que irá verificar quantas vezes um número aparece dentro de um array
 function getOccurrence(array, value) {
   var count = 0;
   array.forEach((v) => v === value && count++);
   return count;
 }
 
-console.log(getOccurrence(arr, 1));
-console.log(getOccurrence(arr, 3));
+//Função que irá verificar se o número aleatório já foi atribuído para alguma div 2 vezes, caso não tenha irá o atribuir.
+function checarNumero(numero) {
+  let temp = Math.floor(Math.random() * 8 + 1);
+  if (getOccurrence(arr, temp) < 2) {
+    arr.unshift(temp);
+    numero.innerHTML = temp;
+  } else {
+    checarNumero(numero);
+  }
+}
+
+//Função que irá comparar os valores dentro das duas divs selecionadas.
+
+function comparaSelecionados(arrSelecionados) {
+  console.log(arrSelecionados);
+  if (arrSelecionados[0].innerHTML === arrSelecionados[1].innerHTML) {
+    console.log('Os dois são iguais');
+    arrSelecionados.forEach((e) => {
+      e.classList.add('correto');
+    });
+    // arrSelecionados[0].style.background = 'green';
+    // arrSelecionados[1].style.background = 'green';
+    arrSelecionados.forEach((e) => {
+      e.classList.remove('selecionado');
+    });
+  } else {
+    arrSelecionados.forEach((e) => {
+      e.classList.remove('selecionado');
+      e.classList.add('errado');
+      setTimeout(console.log('eai'), 5000);
+    });
+  }
+}
+
+//! Execução da bagaça -------------------------------------
+
+bolasSelecao.forEach((numero) => {
+  if (numero.innerHTML === '?') {
+    checarNumero(numero);
+  }
+});
+
+// function checarSelecionados() {}
+
+bolasSelecao.forEach((numero_bolas) => {
+  numero_bolas.addEventListener('click', () => {
+    numero_bolas.classList.toggle('selecionado');
+    //Selecionando
+    let arrSelecionados = document.querySelectorAll('.selecionado');
+    if (arrSelecionados.length === 2) {
+      comparaSelecionados(arrSelecionados);
+    }
+  });
+});
